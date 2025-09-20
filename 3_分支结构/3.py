8a65f555031bc9f65b1ac74efbbd021fc5ea9c21
Methry <147138963+qzddmyc@users.noreply.github.com>
2025-09-20 22:11:16 +0800

    250920 optimize

--- 3_分支结构/3.py
+++ 3_分支结构/3.py
@@ -1,10 +1,9 @@
 dis = int(input("距离: "))
-cost = 0
+cost = 10
 if dis > 10:
     cost += (dis - 10) * 1.5
     dis = 10
 if dis > 3:
     cost += (dis - 3) * 1.2
     dis = 3
-cost += 10
 print(f'费用: {cost:.1f}')
