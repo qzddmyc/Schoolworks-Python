d04a3624c6b952f7968f5f2fe5da7881571d1ecb
Methry <147138963+qzddmyc@users.noreply.github.com>
2025-09-26 21:33:09 +0800

    add some details

--- 4_循环一/4.py
+++ 4_循环一/4.py
@@ -5,3 +5,14 @@ RND1, RND2 = r(0, 100), r(0, 100)
 print(f"随机整数 RND1 = {RND1}, RND2 = {RND2}")
 print(f"最大公约数 = {gcd(RND1, RND2)}")
 print(f"最小公倍数 = {lcm(RND1, RND2)}")
+
+
+# 造个轮子
+def GCD(a, b):
+    if b == 0:
+        return a
+    return GCD(b, a % b)
+
+
+def LCM(a, b):
+    return a * b // GCD(a, b)
